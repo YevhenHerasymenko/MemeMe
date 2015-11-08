11bba1ddda303d576100212acfdd5a1caf850933
YevhenHerasymenko <fess2036@ya.ru>
2015-11-08 16:44:22 +0200

    feat: config tableView cell

--- MemeMe/MemeMe/Controllers/SentMemesTableViewController.swift
+++ MemeMe/MemeMe/Controllers/SentMemesTableViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class SentMemesTableViewController: UIViewController {
+class SentMemesTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    //MARK: Table View Data Source
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("sentMemeCell") as! SentMemesTableViewCell
+        
+        cell.config((UIApplication.sharedApplication().delegate as! AppDelegate).memes[indexPath.row])
+        
+        return cell
+    }
+    
+    //MARK: Table View Delegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
